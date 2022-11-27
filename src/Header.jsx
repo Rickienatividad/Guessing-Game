7c49d4d7297090a43c5c5733e4c415c4e0f426b8
Rickie Natividad <rickienatividad85@gmail.com>
2022-11-26 20:39:55 -0500

    Adds header and title components to start building basic structure

---
+++ src/Header.jsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <div>
+      <h3>Reset!</h3>
+      <h5>Numbers Between 1 and 20 only!!</h5>
+    </div>
+  );
+}
---
+++ src/Title.jsx
@@ -0,0 +1,7 @@
+export function Title() {
+  return (
+    <div>
+      <h1>Guess My Number!</h1>
+    </div>
+  );
+}
